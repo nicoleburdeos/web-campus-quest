6bfed58d3dbc38ca99b3bbc7a403ced479f19619
Josh Evangelista <evangelistajoshlyle@gmail.com>
2025-04-24 23:04:21 +0800

    Form design pa lang

--- src/views/NewView.vue
+++ src/views/NewView.vue
@@ -1,28 +1,85 @@
 <script setup>
 import DashboardView from './auth/DashboardView.vue'
 import { ref } from 'vue'
-
 </script>
 
 <template>
   <DashboardView>
     <v-container fluid>
-      <v-main>
-        <div class="main-content">
-          <v-col cols="12" md="6" lg="6">
-            <v-card class="mx-auto glass-card" elevation="0">
-              <v-img src="/images/cq-logo.png" :height="mobile ? '100' : '70'"></v-img>
-              <v-card-title class="text-center">
-                <h3 class="font-weight-black">New Task</h3>
-              </v-card-title>
-              <!-- Example placeholder for tasks -->
-              <v-card-text>
-                <div class="text-center">No tasks to display.</div>
-              </v-card-text>
-            </v-card>
-          </v-col>
-        </div>
-      </v-main>
+      <v-row justify="center">
+        <v-col cols="12" md="5">
+          <v-card class="mx-auto glass-card" elevation="0">
+            <v-img src="/images/cq-logo.png" height="70"></v-img>
+            <v-card-title class="text-center">
+              <h3 class="font-weight-black">New Task</h3>
+            </v-card-title>
+            <v-card-text>
+              <v-form>
+                <v-container>
+                  <v-row>
+                    <v-col cols="12">
+                      <v-text-field
+                        hint="For example, foods, school supplies, etc"
+                        label="Task Name"
+                        variant="underlined"
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="12" sm="6">
+                      <v-text-field
+                        hint="For example, Kinaadman Bldg, Hiraya Bldg , etc"
+                        label="Pickup Point"
+                        prepend-inner-icon="mdi-map-marker"
+                        variant="underlined"
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="12" sm="6">
+                      <v-text-field
+                        hint="For example, Kinaadman Bldg, Hiraya Bldg , etc"
+                        label="Destination"
+                        prepend-inner-icon="mdi-map-marker"
+                        variant="underlined"
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="12" sm="6">
+                      <v-select
+                        label="Task Type"
+                        :items="['Food Delivery', 'Item Delivery', 'Ride Hailing']"
+                        variant="outlined"
+                      ></v-select>
+                    </v-col>
+                    <v-col cols="12" sm="6">
+                      <v-select
+                        label="Payment Method"
+                        :items="['G-cash', 'Cash On Delivery']"
+                        variant="outlined"
+                      ></v-select>
+                    </v-col>
+                    <v-col cols="12" sm="6">
+                      <v-text-field
+                        label="Quantity"
+                        model-value="1"
+                        variant="outlined"
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="12" sm="6">
+                      <v-text-field
+                        label="Price"
+                        model-value="Php "
+                        variant="outlined"
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="12" class="text-center">
+                      <v-btn prepend-icon="mdi-check" color="green-darken-4">
+                        Submit
+                      </v-btn>
+                    </v-col>
+                  </v-row>
+                </v-container>
+              </v-form>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
     </v-container>
   </DashboardView>
 </template>
