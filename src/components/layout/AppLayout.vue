b9cd3ca2b1d720a5fdbb069a8885f62aab05f5f2
cjdumago <cjdumago7@gmail.com>
2025-04-26 13:27:38 +0800

    Initial commit

---
+++ src/components/layout/AppLayout.vue
@@ -0,0 +1,48 @@
+<script setup>
+import { ref } from 'vue'
+
+const theme = ref('light')
+
+// function onClick() {
+//   theme.value = theme.value === 'light' ? 'dark' : 'light'
+// }
+</script>
+<template>
+  <v-responsive class="border rounded">
+    <v-app :theme="theme">
+      <v-app-bar class="px-2" color="green-darken-4" height="100" app>
+        <v-img
+          src="/images/cq-logo-text.png"
+          max-height="500"
+          max-width="300"
+          class="mt-7"
+          contain
+        ></v-img>
+        <v-spacer></v-spacer>
+
+        <!-- <v-btn
+          :icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'"
+          variant="elevated"
+          color="green-darken-3"
+          slim
+          @click="onClick"
+        ></v-btn> -->
+      </v-app-bar>
+      <!-- Background image added here -->
+      <v-main
+        style="
+          background-image: url('/images/bg-img.png');
+          background-size: cover;
+          background-position: center;
+          min-height: 100vh;
+        "
+      >
+        <slot name="content"></slot>
+      </v-main>
+
+      <v-footer class="font-weight-bold" color="green-darken-4" border app>
+        Copyright © 2025 Campus Quest | All rights reserved.</v-footer
+      >
+    </v-app>
+  </v-responsive>
+</template>
---
+++ src/views/auth/RegisterView.vue
@@ -0,0 +1,37 @@
+<script setup>
+import AppLayout from '@/components/layout/AppLayout.vue'
+import RegistrationForm from '@/components/auth/RegistrationForm.vue'
+import { useDisplay } from 'vuetify'
+
+const { mobile } = useDisplay()
+</script>
+
+<template>
+  <AppLayout>
+    <template #content>
+      <v-row>
+        <v-col cols="12" md="6" class="mx-auto pt-16">
+          <v-card class="mx-auto" elevation="24">
+            <v-card-title class="text-center">
+              <v-img src="/images/cq-logo.png" :height="mobile ? '150' : '100'"></v-img>
+              <h2 class="font-weight-black">Campus Quest</h2>
+              <p class="font-weight-bold">Registration Form</p>
+            </v-card-title>
+            <v-card-text class="bg-surface-light pt-4">
+              <v-divider class="my-5"></v-divider>
+              <RegistrationForm></RegistrationForm>
+
+              <v-divider class="my-5"></v-divider>
+              <h4 class="text-center">
+                Already have an account?
+                <router-link class="text-green-darken-1 font-weight-black" to="/"
+                  >Click here to login</router-link
+                >
+              </h4>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </template>
+  </AppLayout>
+</template>
