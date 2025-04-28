ab8c5804824230af37a4be3ea31127e4d81b599d
Josh Evangelista <evangelistajoshlyle@gmail.com>
2025-04-28 18:48:41 +0800

    feat/adding task info template

--- src/views/auth/TaskboardView.vue
+++ src/views/auth/TaskboardView.vue
@@ -19,34 +19,79 @@ const items = Array.from({ length: 6 }, (k, v) => v + 1)
               </v-card-title>
               <!-- Example placeholder for tasks -->
               <v-card-text>
-                <v-virtual-scroll
-      :items="items"
-      height="320"
-      item-height="20"
-    >
-  
-      <template v-slot:default="{ item }">
-        <v-list-item
-          :subtitle="`Badge #${item}`"
-          :title="'Task Name'"
-        > 
-          <template v-slot:prepend>
-            <v-icon class="bg-primary">mdi-account</v-icon>
-          </template>
+                <v-virtual-scroll :items="items" height="320" item-height="20">
+                  <template v-slot:default="{ item }">
+                    <v-list-item :subtitle="`Badge #${item}`" :title="'Task Name'">
+                      <template v-slot:prepend>
+                        <v-icon>mdi-account</v-icon>
+                      </template>
 
-          <template v-slot:append>
-            <v-btn
-              icon="mdi-open-in-new"
-              size="x-small"
-              variant="tonal"
-            ></v-btn>
-          </template>
-        </v-list-item>
-        <v-divider></v-divider>
-        <br>
-      </template>
-     
-    </v-virtual-scroll>
+                      <template v-slot:append>
+                        <v-btn icon variant="plain" size="small" class="info-btn">
+                          <v-icon>mdi-information</v-icon>
+                          <v-dialog activator="parent" max-width="500">
+                            <template v-slot:default="{ isActive }">
+                              <v-card rounded="lg">
+                                <v-card-title class="d-flex justify-space-between align-center">
+                                  <div class="text-h5 text-medium-emphasis ps-2">Task Info</div>
+                                  <v-btn
+                                    icon="mdi-close"
+                                    variant="text"
+                                    @click="isActive.value = false"
+                                  ></v-btn>
+                                </v-card-title>
+                                <v-divider class="mb-4"></v-divider>
+                                <v-card-text>
+                                  <div class="text-medium-emphasis mb-4">
+                                    Invite collaborators to your network and grow your connections.
+                                  </div>
+                                  <div class="mb-2">Message (optional)</div>
+                                  <v-textarea
+                                    :counter="300"
+                                    class="mb-2"
+                                    rows="2"
+                                    variant="outlined"
+                                    persistent-counter
+                                  ></v-textarea>
+                                  <div class="text-overline mb-2">💎 PREMIUM</div>
+                                  <div class="text-medium-emphasis mb-1">
+                                    Share with unlimited people and get more insights about your
+                                    network. Try Premium Free for 30 days.
+                                  </div>
+                                  <v-btn
+                                    class="text-none font-weight-bold ms-n4"
+                                    color="primary"
+                                    text="Retry Premium Free"
+                                    variant="text"
+                                  ></v-btn>
+                                </v-card-text>
+                                <v-divider class="mt-2"></v-divider>
+                                <v-card-actions class="my-2 d-flex justify-end">
+                                  <v-btn
+                                    class="text-none"
+                                    rounded="xl"
+                                    text="Cancel"
+                                    @click="isActive.value = false"
+                                  ></v-btn>
+                                  <v-btn
+                                    class="text-none"
+                                    color="green"
+                                    rounded="xl"
+                                    text="Accept"
+                                    variant="flat"
+                                    @click="isActive.value = false"
+                                  ></v-btn>
+                                </v-card-actions>
+                              </v-card>
+                            </template>
+                          </v-dialog>
+                        </v-btn>
+                      </template>
+                    </v-list-item>
+                    <v-divider></v-divider>
+                    <br />
+                  </template>
+                </v-virtual-scroll>
               </v-card-text>
             </v-card>
           </v-col>
