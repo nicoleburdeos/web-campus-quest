a8e8b9043560d44f762b0acdaf3f5c3911f7e1a7
cjdumago <cjdumago7@gmail.com>
2025-04-28 13:10:57 +0800

    fix: navigation drawer mobile lenght

--- src/views/auth/DashboardView.vue
+++ src/views/auth/DashboardView.vue
@@ -22,14 +22,13 @@ const drawer = ref(true)
             >
               <v-icon>{{ drawer ? 'mdi-close' : 'mdi-menu' }}</v-icon>
             </v-btn>
-<br>
+            <br />
             <v-navigation-drawer
               v-model="drawer"
               color="green-darken-4"
-              expand-on-hover
-              rail
-              app
               :temporary="$vuetify.display.smAndDown"
+              :width="$vuetify.display.smAndDown ? '250' : undefined"
+              class="mx-auto"
             >
               <v-list>
                 <v-list-item
